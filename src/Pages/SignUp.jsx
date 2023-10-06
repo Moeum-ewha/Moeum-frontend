db20b6ddd0997e87f2a380aa4ec89fc18921c643
ohjinyxung <esujie339@ewhain.net>
2023-10-06 15:58:07 +0900

    [add]회원가입 페이지

---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from "react";
+import { useNavigate } from "react-router-dom";
+
+import BackgroundContainer from "../Components/BackgroundContainer";
+import {  } from "../Components/SignUpComponents";
+
+const SignUp = () => {
+    return (
+     <BackgroundContainer>
+      </BackgroundContainer>
+    );
+};
+
+export default SignUp;

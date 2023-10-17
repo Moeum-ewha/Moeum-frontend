1cc2f6c26f0b56ab3e265593f4ffce5e6df0fd4f
ohjinyxung <esujie339@ewhain.net>
2023-10-18 02:02:52 +0900

    add/홈화면(미완)

---
+++ src/Components/HomeComponents.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { styled } from "styled-components";
+
+export const Content = styled.div`
+    width: 393px;
+    height: 852px;
+`;
+
+export const TopBar = styled.div`
+    height: 150px;
+`;
+
+export const Title = styled.div`
+    font-family: 'Arial';
+    font-style: normal;
+    font-weight: 300;
+    font-size: 18px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+`;
+
+export const Alert = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+`;
+
+export const LogoC = styled.div`
+
+`;
+
+export const Balloon = styled.div`
+
+`;
+
+export const Gallery = styled.div`
+
+`;
+
+
+
+
+

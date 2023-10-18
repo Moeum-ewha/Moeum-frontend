fbfabb3ac82f2ea7e20ebd89f6f96843e73a5180
ohjinyxung <esujie339@ewhain.net>
2023-10-19 03:26:20 +0900

    add/홈화면

--- src/Components/HomeComponents.jsx
+++ src/Components/HomeComponents.jsx
@@ -4,30 +4,35 @@ import { styled } from "styled-components";
 export const Content = styled.div`
     width: 393px;
     height: 852px;
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
 `;
 
 export const TopBar = styled.div`
-    height: 150px;
+    height: 170px;
 `;
 
 export const Title = styled.div`
     font-family: 'Arial';
     font-style: normal;
-    font-weight: 300;
-    font-size: 18px;
-    display: flex;
-    align-items: center;
-    text-align: center;
+    font-weight: 120;
+    font-size: 20px;
+    margin-top: 50px;
 `;
 
 export const Alert = styled.div`
     display: flex;
-    justify-content: center;
     flex-direction: row;
+    justify-content: center;
+    margin-top: 20px;
 `;
 
 export const LogoC = styled.div`
-
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width:80px;
 `;
 
 export const Balloon = styled.div`
@@ -35,10 +40,42 @@ export const Balloon = styled.div`
 `;
 
 export const Gallery = styled.div`
+    width: 350px;
+    height: 650px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+`;
 
+export const Photo = styled.div`
+    width: 160px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding-bottom: 15px;
+    filter: drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.25));
+    border-radius: 8px;
 `;
 
 
+export const Left = styled.div`
+    width: 175px;
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+`;
 
+export const Right = styled.div`
+    width: 175px;
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+`;
 
+export const Album = styled.div`
+    width: 175px;
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+`;
 

08e130d89a061b017e39795bfbc212c4140ea7da
GeonHeeAhn <justamoment@ewhain.net>
2023-11-15 03:23:29 +0900

    feat: create URItoBlob function

--- src/Pages/WritePost.jsx
+++ src/Pages/WritePost.jsx
@@ -28,6 +28,25 @@ import dummy2 from '../Assets/yujin2.jpeg';
 import dummy3 from '../Assets/hyejoon2.jpeg';
 import dummy4 from '../Assets/unknown.jpeg';
 
+function dataURItoBlob(dataURI) {
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  let byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0)
+    byteString = atob(dataURI.split(',')[1]);
+  else byteString = decodeURIComponent(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  // write the bytes of the string to a typed array
+  let ia = new Uint8Array(byteString.length);
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ia], { type: mimeString });
+}
+
 export const WritePost = () => {
   const navigate = useNavigate();
   const location = useLocation();
@@ -39,7 +58,6 @@ export const WritePost = () => {
   //newFriendData의 값을 친구 목록 리스트에 저장 -> 각 인물 게시글 db에 원본사진 추가
   //savedFriendData 값에 존재하는 친구들의 각 인물 게시글 db에 원본사진 추가
 
-  // const croppedFaceDataURL = location.state.img;
   const imgURL = location.state.wholeImg;
 
   const [date, setDate] = useState('');

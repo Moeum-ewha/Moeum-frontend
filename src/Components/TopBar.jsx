a05b5e7820b8d4dd332847b021eeed310a021b73
ohjinyxung <esujie339@ewhain.net>
2023-11-16 18:33:52 +0900

    chore/뒤로가기 설정 및 디자인 수정

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -1,4 +1,29 @@
 import { styled } from "styled-components";
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import BackIcon from '../Assets/icons/goback.png';
+
+export const Back = () => {
+  const navigate = useNavigate();
+
+
+  return (
+    <StyledBackButton onClick={() => {navigate(-2)}}>
+      <img src={BackIcon} alt="뒤로가기" />
+    </StyledBackButton>
+  );
+};
+
+export const Back2 = () => {
+  const navigate = useNavigate();
+
+
+  return (
+    <StyledBackButton2 onClick={() => {navigate(-1)}}>
+      <img src={BackIcon} alt="뒤로가기" />
+    </StyledBackButton2>
+  );
+};
 
 export const TopBar = styled.div`
 display: flex;
@@ -13,12 +38,22 @@ export const Title = styled.div`
   margin-top: 50px;
 `;
 
-export const Back = styled.button`
+export const StyledBackButton = styled.button`
   width: 25px;
   height: 25px;
   border: none;
   background:transparent;
   position:absolute;
   right:180px;
+  top: 47px;
+`;
+
+export const StyledBackButton2 = styled.button`
+  width: 25px;
+  height: 25px;
+  border: none;
+  background:transparent;
+  position:absolute;
+  left: 20px;
   top: 47px;
 `;

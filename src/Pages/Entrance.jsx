57eada6adebca2dfa54405e60684b74ed7f9e1ed
ohjinyxung <esujie339@ewhain.net>
2023-11-20 00:46:31 +0900

    진입화면 수정

--- src/Pages/Entrance.jsx
+++ src/Pages/Entrance.jsx
@@ -1,21 +1,23 @@
 import { styled } from "styled-components";
 import Logo from "../Assets/logo.png";
+import React, { useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import BackgroundContainer from "../Components/BackgroundContainer";
 
 export const Entrance = () => {
   const navigate = useNavigate();
 
-  const moveFunc = () => {
-    navigate("login");
-  };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/login');
+    }, 1500); 
+    return () => clearTimeout(timer);
+  }, [navigate]);
 
   return (
     <Background>
-      <div onClick={moveFunc}>
         <img src={Logo} alt="로고" width="75vw" />
         <LogoText>MOEUM</LogoText>
-      </div>
     </Background>
   );
 };
@@ -23,6 +25,17 @@ export const Entrance = () => {
 export default Entrance;
 
 export const Background = styled(BackgroundContainer)`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background: #fffcf4;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   background: #ffe8a6;
 `;
 export const LogoText = styled.div`

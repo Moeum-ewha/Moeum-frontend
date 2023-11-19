16fba142930c1359a05d7c34e7b3ca2850cafa31
GeonHeeAhn <justamoment@ewhain.net>
2023-11-19 22:50:42 +0900

    feat: edit date format

--- src/Pages/WritePost.jsx
+++ src/Pages/WritePost.jsx
@@ -226,7 +226,7 @@ export const WritePost = () => {
         <DatePicker
           shouldCloseOnSelect
           locale={ko}
-          dateFormat="yyyy. MM. dd"
+          dateFormat="yyyy-MM-dd"
           selected={startDate}
           onChange={(date) => {
             setStartDate(date);

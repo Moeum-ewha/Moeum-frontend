78e5b63e4e700e5da3449db3a97b5c4256cfe9b6
ohjinyxung <esujie339@ewhain.net>
2023-11-21 05:42:24 +0900

    친구 고르기, 공유 뷰, 링크 수정

--- src/Pages/ShareView.jsx
+++ src/Pages/ShareView.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate, useLocation, useParams } from 'react-router-dom';
+import axios from 'axios';
 
 //components
-import { NavBar, NavBtn, CenterBtn } from '../Components/NavBar';
 import BackgroundContainer from '../Components/BackgroundContainer';
-import { Loading } from './Loading';
-import Main from '../Components/Main';
 import { TopBar, Title, Back } from '../Components/TopBar';
 import {
   Content,
@@ -20,26 +18,24 @@ import {
 } from '../Components/viewingComponents';
 import {CommentSection, Comments, Comment, CommentInputContainer, CommentInput, CommentButton, ProfilePicture, Nickname, CommentContent, CommentDate, CommentContents } from '../Components/Comment'
 import demo from '../../public/dummy/dummy.json';
+import Loading from './Loading';
 
 //assets
-import BackIcon from '../Assets/icons/goback.png';
 import arrow from '../Assets/icons/arrow.png'
 
 export const ShareView = () => {
   const { postId } = useParams(); // URL에서 postId 가져오기
-
-  const postData = demo.userList
-  .flatMap(user => user.postList) // 모든 유저의 postList를 하나의 배열로 합침
-  .find(post => post.id === parseInt(postId)); // postId와 일치하는 포스트 찾음
-
+  const [loading, setLoading] = useState(true);
   const location = useLocation();
+  const postRef = useRef({ current: '' });
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState(null);
+  let pd;
 
-  const postIds = demo.userList.map(post => post.id);
-  const commentList = demo.userList.flatMap(user => 
+const postIds = demo.userList.map(post => post.id);
+/* const commentList = demo.userList.flatMap(user => 
     user.postList.filter(post => postIds.includes(post.id))
-    .flatMap(post => post.commentList));
+    .flatMap(post => post.commentList)); */
 
   const nicknames = ["수줍은 토끼", "감기걸린 토끼", "졸린 토끼", "할로윈 토끼", "왹져 토끼", "수줍은 거북이", "감기걸린 거북이", "졸린 거북이", "할로윈 거북이", "왹져 거북이", "수줍은 강아지", "감기걸린 강아지", "졸린 강아지", "할로윈 강아지", "왹져 강아지", "수줍은 고양이", "감기걸린 고양이", "졸린 고양이", "할로윈 고양이", "왹져 고양이", "수줍은 판다", "감기걸린 판다", "졸린 판다", "할로윈 판다", "왹져 판다" ];
   const profilePictures = ['../../public/profileImg/0.png', '../../public/profileImg/1.png','../../public/profileImg/2.png','../../public/profileImg/3.png','../../public/profileImg/4.png','../../public/profileImg/5.png','../../public/profileImg/6.png','../../public/profileImg/7.png','../../public/profileImg/8.png','../../public/profileImg/9.png','../../public/profileImg/10.png','../../public/profileImg/11.png','../../public/profileImg/12.png','../../public/profileImg/13.png','../../public/profileImg/14.png','../../public/profileImg/15.png','../../public/profileImg/16.png','../../public/profileImg/17.png','../../public/profileImg/18.png','../../public/profileImg/19.png','../../public/profileImg/20.png','../../public/profileImg/21.png','../../public/profileImg/22.png','../../public/profileImg/23.png','../../public/profileImg/24.png', ];
@@ -62,40 +58,89 @@ export const ShareView = () => {
     }
   };
 
+  const sendApi = async () => {
+    try {
+      // Send API request
+      const response = await axios({
+        method: 'GET',
+        url: `/post/${postId}?userId=${13}`,
+        withCredentials: true,
+      });
+      postRef.current = response.data.post;
+      console.log('sendApi = ' + response.status);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        if (error.response) {
+          // Non-2XX status code
+          console.error(error.response.status);
+          console.error(error.response.data);
+        } else if (error.request) {
+          // Request made, no response
+          console.error(error.request);
+        }
+      } else {
+        // Other unexpected error
+        console.error(error);
+      }
+    }
+  };
+
+  const imgApi = async () => {
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: `/images/${postRef.current.imgPath}`,
+        withCredentials: true,
+        responseType: 'blob',
+      });
+
+      console.log('imgApi = ' + response.status);
+      const blobUrl = URL.createObjectURL(new Blob([response.data]));
+
+      postRef.current = { ...postRef.current, imgPath: blobUrl };
+    } catch (error) {
+      // 오류 처리
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await sendApi();
+      await imgApi();
+    };
+
+    fetchData();
+  }, []);
+
+
   return (
         <BackgroundContainer>
           <Content>
           <TopBar>
             <Title>네컷 일기</Title>
           </TopBar>
+          {loading ? (
+          <Loading />
+        ) : (
+          <>
           <ContentContainer>
             <MiniContainer>
               <ImgContainer>
-              <img src={`../../dummy/${postData.original}`} width="100%" />
+              <img src={postRef.current.imgPath} width="100%" />
               </ImgContainer>
-              <SecondaryTitle>{postData.friendId.map(friendId => {
-                                const friend = demo.userList.flatMap(user => user.friendsList)
-                                    .find(friend => friend.id === friendId);
-                                return friend ? friend.name : '';
-                            }).join(', ')}{'('}이{')'}랑{' '}{postData.location}에서</SecondaryTitle>
-              <Text>{postData.content}</Text>
-              <Date>{postData.date}</Date>
+              <SecondaryTitle>{postRef.current.friends
+                    .map((friend) => (friend ? `${friend.friendName}` : ''))
+                    .join(', ')}
+                  {'('}이{')'}랑 {postRef.current.location}에서</SecondaryTitle>
+              <Text>{postRef.current.content}</Text>
+              <Date>{postRef.current.takenAt}</Date>
             </MiniContainer>
           </ContentContainer>
           <CommentSection>
           <Comments>
-          {commentList.map((comment, index) => (
-            <Comment key={comment.id}>
-          <ProfilePicture alt="프로필 사진" >
-           <img src={profilePictures[comment.name[0]]} width ="30px"/>
-          </ProfilePicture>
-          <CommentContents>
-            <Nickname>{nicknames[comment.name[0]]}</Nickname>
-            <CommentContent>{comment.content}</CommentContent>
-            <CommentDate>{comment.date}</CommentDate>
-          </CommentContents>
-        </Comment>
-        ))}
             {comments.map((comment, index) => (
         <Comment key={index}>
           <ProfilePicture alt="프로필 사진" >
@@ -120,6 +165,8 @@ export const ShareView = () => {
             </CommentButton>
           </CommentInputContainer>
         </CommentSection>
+        </>
+        )}
           </Content>
       </BackgroundContainer>
   )

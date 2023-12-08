d4d603ec247e32cae49e8d30ae340e0ae2f3b69d
GeonHeeAhn <justamoment@ewhain.net>
2023-12-08 16:20:17 +0900

    fix: change HomeScreen.jsx

--- src/Pages/HomeScreen.jsx
+++ src/Pages/HomeScreen.jsx
@@ -17,7 +17,6 @@ import {
   Right,
 } from '../Components/HomeComponents';
 import { NavBar } from '../Components/NavBar';
-import demo from '../../public/dummy/dummy.json';
 import axios, { AxiosError } from 'axios';
 import Loading from './Loading';
 
@@ -30,34 +29,28 @@ const HomeScreen = () => {
   const [name, setName] = useState('');
 
   const sendApi = async () => {
-    // Send 버튼 더블클릭 방지
     if (loading) return;
 
     setLoading(true);
 
     try {
-      // Send API request
       const response = await axios({
         method: 'GET',
         url: `/posts`,
         withCredentials: true,
       });
       setPostlist(response.data.posts);
-      // 2XX status code
       console.log(response.status);
       console.log(response.data);
     } catch (error) {
       if (error instanceof AxiosError) {
         if (error.response) {
-          // Non-2XX status code
           console.error(error.response.status);
           console.error(error.response.data);
         } else if (error.request) {
-          // Request made, no response
           console.error(error.request);
         }
       } else {
-        // Other unexpected error
         console.error(error);
       }
     } finally {
@@ -66,9 +59,7 @@ const HomeScreen = () => {
   };
 
   const imgApi = async () => {
-    console.log('api 들어옴ㅁ');
     try {
-      console.log('try문 들어옴');
       const newPostlist = await Promise.all(
         postlist.map(async (post) => {
           const response = await axios({
@@ -77,22 +68,14 @@ const HomeScreen = () => {
             withCredentials: true,
             responseType: 'blob',
           });
-
-          // 2XX status code
           console.log(response.status);
-
-          // 이미지를 Blob에서 URL로 변환
           const blobUrl = URL.createObjectURL(new Blob([response.data]));
-
-          // 새로운 객체를 생성하여 기존 post의 정보를 복사하고 imgPath를 업데이트
           return { ...post, imgPath: blobUrl };
         }),
       );
 
       setpd(newPostlist);
-      console.log('일단 map은 끝남');
     } catch (error) {
-      // 오류 처리
       console.error(error);
     } finally {
       setLoading(false);
@@ -103,7 +86,7 @@ const HomeScreen = () => {
     try {
       const response = await axios({
         method: 'GET',
-        url: `/latest?userId=${1}`,
+        url: `/latest`,
         withCredentials: true,
       });
 
@@ -111,7 +94,6 @@ const HomeScreen = () => {
       setName(response.data.post.friends[0].friendName);
       console.log(response.data.post.friends[0].friendName);
     } catch (error) {
-      // 오류 처리
       console.error(error);
     }
   };
@@ -164,7 +146,6 @@ const HomeScreen = () => {
                   src={post.imgPath}
                   width="160px"
                   style={{ borderRadius: '15px' }}
-                  // onClick={movePost}
                 />
               </Photo>
             ))
